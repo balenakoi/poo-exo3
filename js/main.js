71ab693215e93e6785f3e308a7b888b9a41a54cb
balen <balenakoi@gmail.com>
2018-11-13 14:48:26 +0100

    finished the exersice

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
 
-
+function Product(title, phrase, description){
+    this.title = title;
+    this.phrase = phrase;
+    this.description = description;
+  }
+  
+  let sumbit = {
+    getInput: function(){
+      let title = document.getElementById("title").value;
+      let phrase = document.getElementById("phrase").value;
+      let description = document.getElementById("description").value;
+      sumbit.newObjet(title, phrase, description);
+    },
+    newObjet: function(title, phrase, description){
+      let element = new Product(title, phrase, description);
+      sumbit.htmlManager(element);
+    },
+    htmlManager: function(element){
+      let newDiv = document.createElement("p");
+      cards.append(newDiv);
+      let title = document.createElement("p");
+      title.innerHTML = element.title;
+      newDiv.append(title);
+      let phrase = document.createElement("p");
+      phrase.innerHTML = element.phrase;
+      newDiv.append(phrase);
+      let description = document.createElement("p");
+      description.innerHTML = element.description;
+      newDiv.append(description);
+      cards = document.getElementById("cards");
+  
+      sumbit.clearAll();
+    },
+    clearAll: function(){
+      document.getElementById("title").value = "";
+      document.getElementById("phrase").value = "";
+      document.getElementById("description").value = "";
+    }
+  };
 
